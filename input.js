440621e9cb1260a16bccaefd0638f81a015a66e7
bofeiw <bofeiw@icloud.com>
2019-05-09 17:55:57 +1000

    add demo

---
+++ input.js
@@ -0,0 +1,26 @@
+adjustSize('circle');
+
+function input() {
+    const text = document.getElementById('input').value;
+    const circleText = document.getElementById('text');
+    circleText.innerHTML = text;
+    adjustSize('circle');
+}
+
+
+function centerCircle() {
+    const circle = document.getElementById('circle');
+    const container = document.getElementById('container');
+
+    const circleWidth = circle.offsetWidth;
+    const containerWidth = container.offsetWidth;
+
+    const centerX = containerWidth / 2;
+    const leftX = centerX - circleWidth / 2;
+
+    circle.style.left = leftX + 'px';
+}
+
+centerCircle();
+
+window.onresize = () => centerCircle();
